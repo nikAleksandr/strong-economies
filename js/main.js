035638de8103786906243a4283983382daf04176
nikAleksandr <njlyell@gmail.com>
2014-06-12 11:07:19 -0400

    map initialized

--- js/main.js
+++ js/main.js
@@ -1,1 +1,112 @@
+d3.select(window).on("resize", throttle);
 
+var zoom = d3.behavior.zoom()
+    .scaleExtent([1, 8])
+    .on("zoom", move);
+
+var width = document.getElementById('container').offsetWidth-60;
+var height = width / 2;
+
+var topo,projection,path,svg,g;
+
+var tooltip = d3.select("#container").append("div").attr("class", "tooltip hidden");
+
+setup(width,height);
+
+function setup(width,height){
+  projection = d3.geo.albersUsa()
+    .translate([0, 0])
+    .scale(width / 2 / Math.PI);
+
+  path = d3.geo.path()
+      .projection(projection);
+
+  svg = d3.select("#map").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+      .call(zoom);
+
+  g = svg.append("g");
+
+}
+
+d3.csv("EDMapData.csv", function (error, countyData) {
+	data = countyData;
+	
+	countyData.forEach(function(d) { 
+	  	TypeById[d.id] = +d.TypeNum; 
+	  	nameById[d.id] = d.countyState;
+	});
+	
+});
+	
+
+d3.json("us.json", function(error, us) {
+
+  var counties = topojson.feature(us, us.objects.counties).features;
+
+  topo = counties;
+  draw(topo);
+
+});
+
+function draw(topo) {
+
+  var county = g.selectAll(".county").data(topo);
+
+  county.enter().insert("path")
+      .attr("class", "county")
+      .attr("d", path)
+      .attr("id", function(d,i) { return d.id; })
+      .style("fill", function(d) { if(!isNaN(RGDPGrowth13ById[d.id])){return color(RGDPGrowth13ById[d.id]);} else{return "#ccc";} });
+
+  //ofsets plus width/height of transform, plsu 20 px of padding, plus 20 extra for tooltip offset off mouse
+  var offsetL = document.getElementById('container').offsetLeft+(width/2)+40;
+  var offsetT =document.getElementById('container').offsetTop+(height/2)+20;
+
+  //tooltips
+  county
+    .on("mousemove", function(d,i) {
+      var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
+        tooltip
+          .classed("hidden", false)
+          .attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
+          .html(d.properties.name);
+      })
+      .on("mouseout",  function(d,i) {
+        tooltip.classed("hidden", true)
+      }); 
+   
+}
+
+function redraw() {
+  width = document.getElementById('container').offsetWidth-60;
+  height = width / 2;
+  d3.select('svg').remove();
+  setup(width,height);
+  draw(topo);
+}
+
+function move() {
+
+  var t = d3.event.translate;
+  var s = d3.event.scale;  
+  var h = height / 3;
+  
+  t[0] = Math.min(width / 2 * (s - 1), Math.max(width / 2 * (1 - s), t[0]));
+  t[1] = Math.min(height / 2 * (s - 1) + h * s, Math.max(height / 2 * (1 - s) - h * s, t[1]));
+
+  zoom.translate(t);
+  g.style("stroke-width", 1 / s).attr("transform", "translate(" + t + ")scale(" + s + ")");
+
+}
+
+var throttleTimer;
+function throttle() {
+  window.clearTimeout(throttleTimer);
+    throttleTimer = window.setTimeout(function() {
+      redraw();
+    }, 200);
+}
